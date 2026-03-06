35b609dfcf0b7744ec68dd060fdb2f28cb6ace3d
Santtos <ruanrodrigues393@gmail.com>
2026-03-06 07:50:13 -0300

    feat: adding initial registers addresses

---
+++ config.h
@@ -0,0 +1,9 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+#include "registers.h"
+#include <stdint.h>
+
+#define RESETS_SET_BIT ((volatile uint32_t *)(RESETS_BASE)) 
+
+#endif
---
+++ registers.h
@@ -0,0 +1,11 @@
+#ifndef REGISTERS_H
+#define REGISTERS_H
+
+#define RESETS_BASE   0x4000c000
+#define SIO_BASE      0xd0000000
+#define IO_BANK0_BASE 0x40014000
+#define ADC_BASE      0x4004c000
+
+#define RESETS_DONE   RESETS_BASE + 0x8
+
+#endif
